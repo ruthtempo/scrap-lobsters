706f020d18eabc7b0316b8c44dd744f485ac28fd
Ruth Temporal <ruthtemp@gmail.com>
2023-01-20 12:21:08 +0100

    filtered most upted categories, usecallback hook, centered screen

--- src/Chart.js
+++ src/Chart.js
@@ -1,23 +1,26 @@
 import { Chart } from "chart.js/auto";
+import { useCallback } from "react";
 
 export const ChartInfo = (props) => {
-  const createChart = (canvas) => {
+  const mostUpvoted = props.data.filter((data) => data.upvotes >= 25);
+
+  const createChart = useCallback((canvas) => {
     new Chart(canvas, {
       type: "pie",
       data: {
-        labels: props.data.map((row) => row.category), //map through categories
+        labels: mostUpvoted.map((row) => row.category), //map through categories
         datasets: [
           {
             label: "upvotes",
-            data: props.data.map((row) => row.upvotes),
+            data: mostUpvoted.map((row) => row.upvotes),
           },
         ],
       },
     });
-  };
+  }, []);
 
   return (
-    <div>
+    <div style={{ width: "50%", margin: "0 auto" }}>
       <canvas ref={createChart}></canvas>
     </div>
   );
